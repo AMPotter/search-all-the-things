1f5a5d2d9da62469e90b5dbcf96a966df82eead2
Andrew Michael Potter <i.am.andrew.potter@gmail.com>
2018-08-16 09:41:07 -0700

    initial commit

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  setupTestFrameworkScriptFile: '<rootDir>/enzyme.setup.js',
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+    '.+\\.(css|styl|less|sass|scss)$': 'jest-css-modules'
+  },
+};
